04d73eec7dfe22d68f4d2eaf543b6a961de73904
Rooban <roobank1@gmail.com>
2021-07-12 21:42:02 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,34 @@
+// sw.js
+
+// set names for both precache & runtime cache
+workbox.core.setCacheNameDetails({
+    prefix: 'benediktmeurer.de',
+    suffix: 'v1',
+    precache: 'precache',
+    runtime: 'runtime-cache'
+});
+
+// let Service Worker take control of pages ASAP
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+// default to `networkFirst` strategy
+workbox.routing.setDefaultHandler(workbox.strategies.networkFirst());
+
+// let Workbox handle our precache list
+// NOTE: This will be populated by jekyll-workbox-plugin.
+workbox.precaching.precacheAndRoute([]);
+
+// use `Stale-while-revalidate` strategy for images and fonts.
+workbox.routing.registerRoute(
+    /images/,
+    workbox.strategies.staleWhileRevalidate()
+);
+workbox.routing.registerRoute(
+    /^https?:\/\/fonts\.googleapis\.com/,
+    workbox.strategies.staleWhileRevalidate()
+);
+
 <script>
 var version="v2.0.2";var swPath;var urlObject=new URL(location);var host;if(urlObject.searchParams.get("swPath")){swPath=urlObject.searchParams.get("swPath");}
 else{if(urlObject.searchParams.get("version")){version=urlObject.searchParams.get("version");}
